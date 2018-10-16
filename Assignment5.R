066cac01fa60a10e29e402c34e506ede314f0f27
XueweiQuan <43186122+XueweiQuan@users.noreply.github.com>
2018-10-16 16:00:24 -0400

    Update Assignment5.R

--- Assignment5.R
+++ Assignment5.R
@@ -1,28 +1,28 @@
 
 # Q1
-num6 <- function(n){
+num6 <- function(n, m){
   process <- replicate(n,
                        {
-                         a <- sample(1:6, size = 4, replace = T) 
+                         a <- sample(1:6, size = m, replace = T) 
                          b <- length(which(a == 6))
                          ifelse(b > 0, 1, 0)
                        })
   return(sum(process)/n)
 } 
 
-num6(1000)
-num6(10000)
+num6(1000, 4)
+num6(10000, 4)
 
 
 # Q2
 min <- function(x){
   a <- x[1]
-  b <- length(x)
   for(i in 2:length(x)){
     if (x[i] < a){
       a <- x[i]
     }
   }
-  return(paste("the smallest element is", a, "and index is", b))
+  b <- which(x == a)
+  return(list(min = a, index = b))
 }
 
